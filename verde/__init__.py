64633d7e0d5cb7ab84a2e7142dee0d6589c34f3b
Leonardo Uieda <leouieda@gmail.com>
2018-04-26 17:53:53 -1000

    Start a ScipyGridder class to experiment with API
    The class implements the sklearn fit - predict interface, which will be
    the basis for all our interpolators. Except that we won't use X, y for
    arguments because no one has data like that.
    
    Seems to work but needs refactoring, documentation and tests

--- verde/__init__.py
+++ verde/__init__.py
@@ -6,6 +6,7 @@ from ._version import get_versions as _get_versions
 # Import functions/classes to make the API
 from .utils import scatter_points, grid_coordinates, profile_coordinates
 from . import datasets
+from .scipy_bridge import ScipyGridder
 
 
 # Get the version number through versioneer
