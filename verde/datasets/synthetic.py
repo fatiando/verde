290ccf48e5d99b53163b35ec5e71be8108400a98
Leonardo Uieda <leouieda@gmail.com>
2018-05-01 13:45:00 -1000

    Make synthetic data generation a gridder class

--- verde/datasets/synthetic.py
+++ verde/datasets/synthetic.py
@@ -2,25 +2,27 @@
 Generators of synthetic datasets.
 """
 import numpy as np
-import pandas as pd
-import xarray as xr
+from sklearn.utils.validation import check_is_fitted
 
 from ..base import BaseGridder
+from ..utils import check_region
 
 
 class CheckerBoard(BaseGridder):
-    """
+    r"""
     Generate synthetic data in a checkerboard pattern.
 
     The mathematical model is:
 
     .. math::
 
-        f(e, n) = a \sin(w_e e^2) \cos(w_n n^2)
+        f(e, n) = a
+        \sin\left(\frac{2\pi}{w_e} e\right)
+        \cos\left(\frac{2\pi}{w_n} n\right)
 
     in which :math:`e` is the easting coordinate, :math:`n` is the northing
     coordinate, :math:`a` is the amplitude, and :math:`w_e` and :math:`w_n` are
-    the wavenumbers in the east and north directions, respectively.
+    the wavelengths in the east and north directions, respectively.
 
     The model will evaluated on random points or on a regular grid depending on
     the value of *scatter*.
@@ -30,16 +32,44 @@ class CheckerBoard(BaseGridder):
     amplitude : float
         The amplitude of the checkerboard undulations.
     w_east : float
-        The wavenumber in the east direction.
+        The wavelength in the east direction. Defaults to half of the West-East
+        size of the evaluating region.
     w_north : float
-        The wavenumber in the north direction.
+        The wavelength in the north direction. Defaults to half of the
+        South-North size of the evaluating region.
+
+    Examples
+    --------
 
     """
 
     def __init__(self, amplitude=1000, w_east=None, w_north=None):
         self.amplitude = amplitude
         self.w_east = w_east
-        seld.w_north = w_north
+        self.w_north = w_north
+
+    def fit(self, region=(0, 5000, 0, 5000)):
+        """
+        Set the region in which the checkerboard will be evaluated.
+
+        If the wavelengths are not supplied, will set them to half of the
+        region extent in each dimension.
+
+        Parameters
+        ----------
+        region : list
+            The boundaries (``[W, E, S, N]``) of a given region in Cartesian or
+            geographic coordinates.
+
+        """
+        check_region(region)
+        w, e, s, n = region
+        if self.w_east is None:
+            self.w_east = (e - w)/2
+        if self.w_north is None:
+            self.w_north = (n - s)/2
+        self.region_ = region
+        return self
 
     def predict(self, easting, northing):
         """
@@ -58,6 +88,8 @@ class CheckerBoard(BaseGridder):
             The evaluate checkerboard function.
 
         """
-        data = self.amplitude*np.sin(self.w_east*easting**2)*np.cos(
-            self.w_north*northing**2)
+        check_is_fitted(self, ['region_'])
+        data = (self.amplitude*
+                np.sin((2*np.pi/self.w_east)*easting)*
+                np.cos((2*np.pi/self.w_north)*northing))
         return data
