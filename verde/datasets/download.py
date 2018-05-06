72139b191ce57473dc9a43ccb722174c7f5054fc
Leonardo Uieda <leouieda@gmail.com>
2018-05-05 23:18:20 -1000

    Function to download sample data  (#26)
    Adds function `fetch_data` to get the path of a data file from the data 
    directory (`$HOME/.verde/data`). If the file is not present, download it
    from Github. 
    Uses the requests library to download. In the future, this will allow us
    to implement chunked downloads of larger files.

---
+++ verde/datasets/download.py
@@ -0,0 +1,44 @@
+"""
+Functions to download, verify, and update a sample dataset.
+"""
+import os
+from warnings import warn
+
+import requests
+
+from ..utils import get_data_dir
+
+
+VERDE_DATA_STORE_URL = 'https://github.com/fatiando/verde/raw/master/data'
+
+
+def fetch_data(filename, force_download=False):
+    """
+    Get the path to a data file in the Verde data directory.
+
+    If it doesn't exist, download it from the remote data store.
+
+    Parameters
+    ----------
+    filename : str
+        The name of the data file to fetch.
+    force_download : bool
+        If True, will download the file even if it already exists.
+
+    Return
+    ------
+    data_path : str
+        A full path to the local file in the data cache directory.
+
+    """
+    data_dir = get_data_dir()
+    data_path = os.path.join(data_dir, filename)
+    if not os.path.exists(data_path) or force_download:
+        data_src = '/'.join([VERDE_DATA_STORE_URL, filename])
+        warn("Downloading data file '{}' from remote data store '{}' to '{}'."
+             .format(filename, VERDE_DATA_STORE_URL, data_dir))
+        response = requests.get(data_src)
+        response.raise_for_status()
+        with open(data_path, 'wb') as dest:
+            dest.write(response.content)
+    return data_path
--- verde/utils.py
+++ verde/utils.py
@@ -1,8 +1,48 @@
 """
 General utilities for dealing with grids and point generation.
 """
+import os
 import numpy as np
 from sklearn.utils import check_random_state
+
+
+def get_home():
+    """
+    Get the path of the verde home directory.
+
+    Defaults to ``$HOME/.verde``.
+
+    If the folder doesn't already exist, it will be created.
+
+    Returns
+    -------
+    path : str
+        The path of the home directory.
+
+    """
+    home = os.path.abspath(os.environ.get('HOME'))
+    verde_home = os.path.join(home, '.verde')
+    os.makedirs(verde_home, exist_ok=True)
+    return verde_home
+
+
+def get_data_dir():
+    """
+    Get the path of the verde data directory.
+
+    Defaults to ``get_home()/data``.
+
+    If the folder doesn't already exist, it will be created.
+
+    Returns
+    -------
+    path : str
+        The path of the data directory.
+
+    """
+    data_dir = os.path.join(get_home(), 'data')
+    os.makedirs(data_dir, exist_ok=True)
+    return data_dir
 
 
 def check_region(region):
