9bdc4a88eba226f924a2aa0cecedba1e742f9d21
Leonardo Uieda <leouieda@gmail.com>
2018-05-05 22:37:21 -1000

    Only need one function not two
    Gave up on the whole local dir thing. Better to just copy data files to
    data dir.

--- verde/datasets/download.py
+++ verde/datasets/download.py
@@ -2,7 +2,6 @@
 Functions to download, verify, and update a sample dataset.
 """
 import os
-import shutil
 from warnings import warn
 from urllib.request import urlopen
 
@@ -12,19 +11,18 @@ from ..utils import get_data_dir
 VERDE_DATA_STORE_URL = 'https://github.com/fatiando/verde/raw/master/data'
 
 
-def fetch_data_from_store(filename):
+def fetch_data(filename, force_download=False):
     """
-    Download data from the Verde data store.
+    Get the path to a data file in the Verde data directory.
 
-    If the environment variable ``VERDE_DATA_STORE`` is set, will fetch data
-    files from there instead. This must be a local absolute path.
-
-    Overwrites the file if it already exists in the Verde data directory.
+    If it doesn't exist, download it from the remote data store.
 
     Parameters
     ----------
     filename : str
-        The name of the data file to fetch. Only the name, not a path.
+        The name of the data file to fetch.
+    force_download : bool
+        If True, will download the file even if it already exists.
 
     Return
     ------
@@ -32,24 +30,13 @@ def fetch_data_from_store(filename):
         A full path to the local file in the data cache directory.
 
     """
-    data_dest = os.path.join(get_data_dir(), filename)
-    # Get data from a local data store if the environment variable is defined
-    data_store = os.environ.get('VERDE_DATA_STORE', None)
-    if data_store is not None:
-        data_src = os.path.join(data_store, filename)
-        if not os.path.exists(data_src):
-            raise FileNotFoundError(
-                "Unable to find data file '{}' at local data store '{}'."
-                .format(filename, data_store))
-        warn("Caching data file '{}' from local data store '{}' to '{}'."
-             .format(filename, data_store, get_data_dir()))
-        shutil.copy2(data_src, data_dest)
-        return data_dest
-    # Download the data from the remote data store
-    data_src = '/'.join([VERDE_DATA_STORE_URL, filename])
-    warn("Downloading data file '{}' from remote data store '{}' to '{}'."
-         .format(filename, VERDE_DATA_STORE_URL, get_data_dir()))
-    with urlopen(data_src) as request:
-        with open(data_dest, 'wb') as dest:
-            dest.write(request.read())
-    return data_dest
+    data_dir = get_data_dir()
+    data_path = os.path.join(data_dir, filename)
+    if not os.path.exists(data_path) or force_download:
+        data_src = '/'.join([VERDE_DATA_STORE_URL, filename])
+        warn("Downloading data file '{}' from remote data store '{}' to '{}'."
+             .format(filename, VERDE_DATA_STORE_URL, data_dir))
+        with urlopen(data_src) as request:
+            with open(data_path, 'wb') as dest:
+                dest.write(request.read())
+    return data_path
--- verde/tests/test_datasets.py
+++ verde/tests/test_datasets.py
@@ -2,49 +2,31 @@
 Test data fetching routines.
 """
 import os
-from pathlib import Path
 import warnings
 from urllib.error import HTTPError
-from contextlib import contextmanager
 
 import pytest
 
-from ..datasets.download import fetch_data_from_store
+from ..datasets.download import fetch_data
 
 
-REPO_DATA_DIR = Path(__file__).parent.parent.parent.joinpath('data')
-
-
-@contextmanager
-def backup_store(path=None):
-    """
-    Backup the VERDE_DATA_STORE environment and remove it or replace with path.
-    Restore it when the exiting the context.
-    Use this to test functions without letting user configuration get in the
-    way.
-    """
-    backup = os.environ.get('VERDE_DATA_STORE', None)
-    if path is not None:
-        os.environ['VERDE_DATA_STORE'] = str(path)
-    else:
-        if backup is not None:
-            del os.environ['VERDE_DATA_STORE']
-    try:
-        yield
-    finally:
-        if backup is not None:
-            os.environ['VERDE_DATA_STORE'] = backup
-
-
-def test_fetch_data_from_store_remote():
+# Has to go first in order to guarantee that the file has been downloaded
+def test_fetch_data_from_remote():
     "Download data from Github to the data directory"
     with warnings.catch_warnings(record=True) as warn:
-        with backup_store():
-            datapath = fetch_data_from_store(
-                'baja-california-bathymetry.csv.xz')
+        datapath = fetch_data('baja-california-bathymetry.csv.xz',
+                              force_download=True)
         assert len(warn) == 1
         assert issubclass(warn[-1].category, UserWarning)
         assert str(warn[-1].message).split()[0] == "Downloading"
+    assert os.path.exists(datapath)
+
+
+def test_fetch_data():
+    "Make sure the file exists when not being downloaded"
+    with warnings.catch_warnings(record=True) as warn:
+        datapath = fetch_data('baja-california-bathymetry.csv.xz')
+        assert len(warn) == 0
     assert os.path.exists(datapath)
 
 
@@ -52,27 +34,7 @@ def test_fetch_data_from_store_remote_fail():
     "Should raise an exception if the remote 404s"
     with warnings.catch_warnings(record=True) as warn:
         with pytest.raises(HTTPError):
-            with backup_store():
-                fetch_data_from_store('invalid remote file name')
+            fetch_data('invalid remote file name')
         assert len(warn) == 1
         assert issubclass(warn[-1].category, UserWarning)
         assert str(warn[-1].message).split()[0] == "Downloading"
-
-
-def test_fetch_data_from_store_local():
-    "Use an environment variable to set data path"
-    with warnings.catch_warnings(record=True) as warn:
-        with backup_store(str(REPO_DATA_DIR)):
-            datapath = fetch_data_from_store(
-                'baja-california-bathymetry.csv.xz')
-        assert len(warn) == 1
-        assert issubclass(warn[-1].category, UserWarning)
-        assert str(warn[-1].message).split()[0] == "Caching"
-    assert os.path.exists(datapath)
-
-
-def test_fetch_data_from_store_local_fail():
-    "An exception should be raised for invalid file names"
-    with pytest.raises(FileNotFoundError):
-        with backup_store(str(REPO_DATA_DIR)):
-            fetch_data_from_store('an invalid file name')
