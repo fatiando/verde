3f2874f48a6c44b79df04d8603eb4804ef8538a2
Leonardo Uieda <leouieda@gmail.com>
2018-06-28 10:24:53 -1000

    Start the data decimation tutorial

--- tutorials/decimation.py
+++ tutorials/decimation.py
@@ -2,55 +2,85 @@
 Data decimation
 ===============
 
-Often times, raw spatial data can be highly oversampled in a direction (shipborne
-and airborne data, for example).
-To avoid aliasing effects,
-it's important to decimate the data to match the desired grid interval before
-interpolation.
-:func:`verde.BlockReduce` decimates
-data by applying a reduction operation (mean, median, mode, max, etc) to the
-data in blocks.
+Often times, raw spatial data can be highly oversampled in a direction. In these cases,
+we need to decimate the data before interpolation to avoid aliasing effects. For
+example, our sample shipborne bathymetry data has a higher sampling frequency along the
+tracks than between tracks:
 """
-###############################################################################
-# First this we need to do is import the ``verde`` library. All of the main
-# functions and classes are available through this single ``import`` (the verde
-# *namespace*). We'll also need a few other libraries for plotting and
-# projecting our data.
-
 import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
-import pyproj
-import numpy as np
 import verde as vd
 
-###############################################################################
-# The first thing to do is load a test data set with which we can work. Verde
-# offers functions for loading our packaged test data in :mod:`verde.datasets`.
-# In this tutorial, we'll work with some bathymetry data from Baja California.
-
+# Load the data as a pandas.DataFrame
 data = vd.datasets.fetch_baja_bathymetry()
 
-###############################################################################
-# The data are stored in a pandas.DataFrame object.
-
-print("Data is of type:", type(data))
-print(data.head())
-
-###############################################################################
-# Plot the data using matplotlib and Cartopy
-
+# Plot it using matplotlib and Cartopy
 crs = ccrs.PlateCarree()
-
-plt.figure(figsize=(7, 6))
+plt.figure(figsize=(7, 7))
 ax = plt.axes(projection=ccrs.Mercator())
-ax.set_title("Bathymetry data from Baja California", pad=25)
+ax.set_title('Locations of bathymetry measurements from Baja California', pad=25)
 # Plot the land as a solid color
-ax.add_feature(cfeature.LAND, edgecolor="black")
-# Plot the bathymetry as colored circles.
-plt.scatter(data.longitude, data.latitude, c=data.bathymetry_m, s=0.1, transform=crs)
-cb = plt.colorbar(pad=0.08)
-cb.set_label("meters")
+ax.add_feature(cfeature.LAND, edgecolor='black')
+# Plot the bathymetry data locations as black dots
+plt.plot(data.longitude, data.latitude, '.k', markersize=1, transform=crs)
+ax.gridlines(draw_labels=True)
+plt.tight_layout()
+plt.show()
+
+########################################################################################
+# Class :class:`verde.BlockReduce` can be used to apply a reduction/aggregation operation (mean,
+# median, standard deviation, etc) to the data in regular blocks.
+# All data inside each block will be replaced by their aggregated value.
+# :class:`~verde.BlockReduce` takes an aggregation function as input.
+# It can be any function that receives a numpy array as input and returns a single
+# scalar value. The :func:`numpy.mean` or :func:`numpy.median` functions are usually
+# what we want.
+import numpy as np
+
+########################################################################################
+# Blocked means and medians are good ways to decimate data for interpolation. Let's use
+# a blocked median on our data to decimate it to our desired grid interval of 5
+# arc-minutes. The reason for using a median over a mean is because bathymetry data can
+# vary abruptly and a mean would smooth the data too much. For data varies more
+# smoothly (like gravity and magnetic data), a mean would be a better option.
+reducer = vd.BlockReduce(reduction=np.median, spacing=5/60)
+print(reducer)
+
+########################################################################################
+# To reduce our data using the blocked median, use the :meth:`~verde.BlockReduce.filter`
+# method:
+coordinates, bathymetry = reducer.filter(coordinates=(data.longitude, data.latitude),
+                                         data=data.bathymetry_m)
+
+plt.figure(figsize=(7, 7))
+ax = plt.axes(projection=ccrs.Mercator())
+ax.set_title('Locations of decimated data', pad=25)
+# Plot the land as a solid color
+ax.add_feature(cfeature.LAND, edgecolor='black')
+# Plot the bathymetry data locations as black dots
+plt.plot(*coordinates, '.k', markersize=1, transform=crs)
+ax.gridlines(draw_labels=True)
+plt.tight_layout()
+plt.show()
+
+
+########################################################################################
+# By default, the coordinates of the decimated data are obtained by applying the same
+# reduction operation to the coordinates of the original data. Alternatively, we can
+# tell :class:`~verde.BlockReduce` to return the coordinates of centers of each block:
+coordinates_center, _ = vd.BlockReduce(reduction=np.median, spacing=5/60,
+                                       center_coordinates=True).filter(coordinates=(data.longitude,
+                                                                                    data.latitude),
+                                                                       data=data.bathymetry_m)
+
+plt.figure(figsize=(7, 7))
+ax = plt.axes(projection=ccrs.Mercator())
+ax.set_title('Locations of decimated data using block centers', pad=25)
+# Plot the land as a solid color
+ax.add_feature(cfeature.LAND, edgecolor='black')
+# Plot the bathymetry data locations as black dots
+plt.plot(*coordinates_center, '.k', markersize=1, transform=crs)
 ax.gridlines(draw_labels=True)
 plt.tight_layout()
 plt.show()
