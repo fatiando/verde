4c50f090bcc741333c9d0ef7140010555362f05a
Leonardo Uieda <leouieda@gmail.com>
2018-06-28 11:04:28 -1000

    Include something about weights
    Not finished yet. Should probably use the vertical GPS data for this
    example.

--- tutorials/decimation.py
+++ tutorials/decimation.py
@@ -84,3 +84,56 @@ plt.plot(*coordinates_center, '.k', markersize=1, transform=crs)
 ax.gridlines(draw_labels=True)
 plt.tight_layout()
 plt.show()
+
+########################################################################################
+# Now the data is ready for interpolation.
+
+########################################################################################
+# Using weights
+# -------------
+#
+# :class:`~verde.BlockReduce` can't output weights for each data point because it
+# doesn't know which reduction operation it's using. If you want to do a weighted
+# interpolation, like :class:`verde.Spline`, you can use :class:`verde.BlockMean`
+# instead. It can calculate different kinds of weights, depending on configuration
+# options and what you give it as input:
+#
+# * If input weights aren't given, then the output weights will be 1 over the variance
+#   of the data in each block.
+# * If input weights are given and they are not related to the uncertainty of the data,
+#   then the output weights will be 1 over the weighted variance of the data in each
+#   block. This is the case when data weights are chosen by the user, not based on the
+#   measurement uncertainty. For example, when you need to give less importance to a
+#   portion of the data and no uncertainties are available.
+# * If input weights are given and they are 1 over the data uncertainty squared, then
+#   use option ``uncertainty=True`` to tell :class:`~verde.BlockMean` to calculate
+#   weights based on the propagated uncertainty of the data. The output weights will be
+#   1 over the propagated uncertainty squared. In this case, the **input weights should
+#   not be normalized**. This is preferable if you have the uncertainty of the data.
+#
+# .. note::
+#
+#    Output weights are always normalized to the ]0, 1] range. See
+#    :func:`verde.variance_to_weights`.
+#
+# Since our bathymetry data doesn't come with uncertainties, we can see what the output
+# weights would look like for options 1.
+coordinates, _, weights_variance = vd.BlockMean(spacing=5/60).filter((data.longitude,
+                                                            data.latitude),
+                                                                     data.bathymetry_m)
+
+# The weights vary a lot so it's better to plot them using a logarithmic color scale
+from matplotlib.colors import LogNorm
+
+plt.figure(figsize=(8, 7))
+ax = plt.axes(projection=ccrs.Mercator())
+ax.set_title('Decimated data weights based on the variance', pad=25)
+# Plot the land as a solid color
+ax.add_feature(cfeature.LAND, edgecolor='black')
+# Plot the output weights as colored circles
+plt.scatter(*coordinates, c=weights_variance, s=1, cmap='magma', transform=crs,
+            norm=LogNorm())
+plt.colorbar(aspect=50)
+ax.gridlines(draw_labels=True)
+plt.tight_layout()
+plt.show()
