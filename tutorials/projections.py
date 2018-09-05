ea54c04f472232e27540259d94c5d50606f97958
Leonardo Uieda <leouieda@gmail.com>
2018-09-05 11:17:02 -1000

    Finish the projections tutorial

--- tutorials/projections.py
+++ tutorials/projections.py
@@ -34,12 +34,13 @@ print(proj_coords)
 ########################################################################################
 # We can plot our projected coordinates using matplotlib.
 
-plt.figure(figsize=(7, 7))
+plt.figure(figsize=(7, 6))
 plt.title("Projected coordinates of bathymetry measurements")
 # Plot the bathymetry data locations as black dots
 plt.plot(proj_coords[0], proj_coords[1], ".k", markersize=0.5)
 plt.xlabel("Easting (m)")
 plt.ylabel("Northing (m)")
+plt.gca().set_aspect("equal")
 plt.tight_layout()
 plt.show()
 
@@ -48,33 +49,36 @@ plt.show()
 # ---------------
 #
 # Now we can use :class:`verde.BlockReduce` and :class:`verde.Spline` on our projected
-# coordinates instead. We'll specify the desired grid spacing as arc-minutes and convert
-# it to Cartesian using the 1 degree = 111 km rule-of-thumb.
-spacing = 7/60
-reducer = vd.BlockReduce(np.median, spacing=spacing*111e3)
+# coordinates. We'll specify the desired grid spacing as degrees and convert it to
+# Cartesian using the 1 degree approx. 111 km rule-of-thumb.
+spacing = 7 / 60
+reducer = vd.BlockReduce(np.median, spacing=spacing * 111e3)
 filter_coords, filter_bathy = reducer.filter(proj_coords, data.bathymetry_m)
 spline = vd.Spline().fit(filter_coords, filter_bathy)
 
 ########################################################################################
 # If we now call :meth:`verde.Spline.grid` we'll get back a grid evenly spaced in
 # projected Cartesian coordinates.
-grid = spline.grid(spacing=spacing*111e3, data_names=["bathymetry"])
+grid = spline.grid(spacing=spacing * 111e3, data_names=["bathymetry"])
+print("Cartesian grid:")
 print(grid)
 
 ########################################################################################
 # We'll mask our grid using :func:`verde.distance_mask` to get rid of all the spurious
 # solutions far away from the data points.
-mask = vd.distance_mask(proj_coords, maxdist=30e3,
-                        coordinates=np.meshgrid(grid.easting, grid.northing))
+mask = vd.distance_mask(
+    proj_coords, maxdist=30e3, coordinates=np.meshgrid(grid.easting, grid.northing)
+)
 grid = grid.where(mask)
 
-plt.figure(figsize=(7, 7))
+plt.figure(figsize=(7, 6))
 plt.title("Gridded bathymetry in Cartesian coordinates")
 plt.pcolormesh(grid.easting, grid.northing, grid.bathymetry, cmap="viridis", vmax=0)
-plt.colorbar(pad=0.1, aspect=50).set_label("bathymetry (m)")
+plt.colorbar().set_label("bathymetry (m)")
 plt.plot(filter_coords[0], filter_coords[1], ".k", markersize=0.5)
 plt.xlabel("Easting (m)")
 plt.ylabel("Northing (m)")
+plt.gca().set_aspect("equal")
 plt.tight_layout()
 plt.show()
 
@@ -83,19 +87,48 @@ plt.show()
 # Geographic grids
 # ----------------
 #
-# The Cartesian grid that we generated won't evenly spaced if we convert the coordinates
-# back to geographic latitude and longitude. Verde gridders allow you to generate an
-# evenly spaced grid in geographic coordinates through the ``projection`` argument of
-# the ``grid`` method.
+# The Cartesian grid that we generated won't be evenly spaced if we convert the
+# coordinates back to geographic latitude and longitude. Verde gridders allow you to
+# generate an evenly spaced grid in geographic coordinates through the ``projection``
+# argument of the :meth:`~verde.base.BaseGridder.grid` method.
 #
-# By providing a projection function (like our ``projection`` variable) to ``grid``,
-# Verde will generate coordinates for a regular grid and then pass them through the
-# projection function before predicting data values. This way, you can generate a grid
-# in a projection that is not the one you used to fit the spline.
+# By providing a projection function (like our pyproj ``projection`` object), Verde will
+# generate coordinates for a regular grid and then pass them through the projection
+# function before predicting data values. This way, you can generate a grid in a
+# projection that is not the one you used to fit the spline.
 
 # Get the geographic bounding region of the data
 region = vd.get_region((data.longitude, data.latitude))
-print(region)
+print("Data region in degrees:", region)
 
+# Specify the region and spacing in degrees and a projection function
+grid_geo = spline.grid(
+    region=region,
+    spacing=spacing,
+    projection=projection,
+    dims=["latitude", "longitude"],
+    data_names=["bathymetry"],
+)
+print("Geographic grid:")
+print(grid_geo)
 
-grid_geo = spline.grid(region=region, spacing=spacing, projection=projection, dims=["
+########################################################################################
+# Now we can use the Cartopy library to plot our geographic grid.
+
+import cartopy.crs as ccrs
+
+plt.figure(figsize=(7, 6))
+ax = plt.axes(projection=ccrs.Mercator())
+ax.set_title("Geographic grid of bathymetry")
+pc = ax.pcolormesh(
+    grid_geo.longitude,
+    grid_geo.latitude,
+    grid_geo.bathymetry,
+    transform=ccrs.PlateCarree(),
+    vmax=0,
+    zorder=-1,
+)
+plt.colorbar(pc).set_label("meters")
+vd.datasets.setup_baja_bathymetry_map(ax)
+plt.tight_layout()
+plt.show()
