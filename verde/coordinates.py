db09719f0dfc5c08567dc2956c6bde42368f1858
Leonardo Uieda <leouieda@gmail.com>
2018-12-19 12:23:59 -1000

    Prototype for a median_distance function
    Has the docstring and one simple test.

--- verde/coordinates.py
+++ verde/coordinates.py
@@ -702,3 +702,56 @@ def block_split(coordinates, spacing, adjust="spacing", region=None):
     tree = kdtree(block_coords)
     labels = tree.query(np.transpose(n_1d_arrays(coordinates, 2)))[1]
     return block_coords, labels
+
+
+def median_distance(coordinates, k_nearest=1, projection=None):
+    """
+    The median horizontal distances between the *k* nearest neighbors of each point.
+
+    For each point specified in *coordinates*, calculate the median of the distance to
+    its *k_nearest* neighbors among the other points in the dataset. Sparse uniformly
+    spaced datasets can use *k_nearest* of 1. Datasets with points clustered into tight
+    groups (e.g., densely sampled along a flight line or ship treck) will have very
+    small distances to the closest neighbors, which is not representative of the actual
+    median spacing because it doesn't take the spacing between lines into account. In
+    these cases, a median of the 10 or 20 nearest neighbors might be more
+    representative.
+
+    The distances calculated are Cartesian (l2-norms). If the coordinates are in
+    geodetic latitude and longitude, provide a *projection* function to convert them to
+    Cartesian before doing the computations.
+
+    Parameters
+    ----------
+    coordinates : tuple of arrays
+        Arrays with the coordinates of each data point. Should be in the
+        following order: (easting, northing, vertical, ...). Only the first two
+        coordinates (assumed to be the horizontal) will be used in the distance
+        computations.
+    k_nearest : int
+        Will calculate the median of the *k* nearest neighbors of each point. A value of
+        1 will result in the distance to nearest neighbor of each data point.
+    projection : callable or None
+        If not None, then should be a callable object (like a function)
+        ``projection(easting, northing) -> (proj_easting, proj_northing)`` that
+        takes in easting and northing coordinate arrays and returns projected
+        northing and easting coordinate arrays.
+
+    Returns
+    -------
+    distances : array
+        An array with the median distances to the *k* nearest neighbors of each data
+        point. Will have the same shape as the first coordinate array provided.
+
+    Examples
+    --------
+
+    >>> import verde as vd
+    >>> coords = vd.grid_coordinates((0, 10, -20, -15), spacing=1)
+    >>> # The nearest neighbor distance should be the grid spacing
+    >>> distance = median_distance(coords, k_nearest=1)
+    >>> print("{:.2f}".format(distance))
+    1.00
+
+    """
+    pass
