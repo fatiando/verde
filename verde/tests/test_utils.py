d68eca4e0c187913b09270dc3cbf386ab1f3c552
Jesse Pisel <jessepisel@users.noreply.github.com>
2019-02-21 14:18:54 -0700

    header parse test
    this creates a fake header as a list then passes it to a helper function that is the first half of the load_surfer function. It returns the header information after parsing it and compares to the original list of strings. Definitely a WIP as I figure out how to write tests. Any feedback is more than welcome.

--- verde/tests/test_utils.py
+++ verde/tests/test_utils.py
@@ -52,3 +52,46 @@ def test_kdtree():
         npt.assert_allclose(dist, 0.1)
         if not use_pykdtree:
             assert isinstance(tree, cKDTree)
+
+
+def test_surfer_header():
+    "Loads and parses the fake surfer header"
+    fake_header = ["DSAA", "100 200", "1.0 1000.0", "2.0 2000.0", "3.0 300.0"]
+
+    def load_surfer_header(fname):
+        with StringIO(fname) as input_file:
+            # DSAA is a Surfer ASCII GRD ID
+            grd_id = input_file.readline().strip()
+            # Read the number of columns (ny) and rows (nx)
+            ydims, xdims = [int(s) for s in input_file.readline().split()]
+            # Our x points North, so the first thing we read is y, not x.
+            south, north = [float(s) for s in input_file.readline().split()]
+            west, east = [float(s) for s in input_file.readline().split()]
+            dmin, dmax = [float(s) for s in input_file.readline().split()]
+
+        return [grd_id, ydims, xdims, south, north, west, east, dmin, dmax]
+
+    test_header = "\n".join(fake_header)
+    grid_id_name = fake_header[0]
+    ydimension = int(fake_header[1].split(" ")[0])
+    xdimension = int(fake_header[1].split(" ")[1])
+    southmin = float(fake_header[2].split(" ")[0])
+    northmax = float(fake_header[2].split(" ")[1])
+    westmin = float(fake_header[3].split(" ")[0])
+    eastmax = float(fake_header[3].split(" ")[1])
+    zmin = float(fake_header[4].split(" ")[0])
+    zmax = float(fake_header[4].split(" ")[1])
+
+    surfer_grd = load_surfer_header(test_header)
+
+    assert surfer_grd == [
+        grid_id_name,
+        ydimension,
+        xdimension,
+        southmin,
+        northmax,
+        westmin,
+        eastmax,
+        zmin,
+        zmax,
+    ]
