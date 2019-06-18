447f72d0c51a78d1c8e7e251f395196b14eeea6d
Jesse Pisel <jessepisel@users.noreply.github.com>
2019-06-18 12:53:01 -0600

    Create grid_coordinates.py
    initial tutorial on how grid_coordinates works. Work in progress, still need to add pixel registration, lint, format, and build with sphinx

---
+++ tutorials/grid_coordinates.py
@@ -0,0 +1,126 @@
+"""
+.. _grid_coordinates:
+
+Gridding coordinates
+====================
+
+Grid coordinates in Verde are used to create points on a regularly spaced grid
+that are  then used in the spline method to interpolate between sample data
+points. The grid can be specified either by the number of points in each
+dimension (the *shape*) or by the grid node spacing.
+
+Creating regular grids in Verde is done using the :func:`verde.grid-coordinates`
+function. The function uses :mod:`numpy.linspace` and
+:mod:`numpy.meshgrid` to interpolate between points in both the west-east and
+south-north directions and creates a two-dimensional spatial grid. To this
+end, the function will adjust either the region bounds or the spacing between
+grid nodes.
+
+When we choose to adjust the region the function takes the original region, and
+divides the south-north and west-east extents of the region by the spacing to
+get the number of grid points in both directions. This value is then
+rounded and converted to an integer. The function then adds one to the integer
+to get the number of nodes rather than segments.The function then multiplies
+the number of grid points by the chosen spacing to create the new region.
+
+Let's create a region that is 1555 units west-east and 1250 units south-north
+with a grid spacing every 500 units. For these dimensions and spacing we should
+expect the west-east dimension to contain 4 grid points (1555/500 is 3.11 which
+is rounded to 3 segments and 4 nodes), and the south-north dimensions to
+contain 3 grid points (1250/500 is 2.5 which is rounded to 2 segments and 3
+nodes. After :func:`verde.grid-coordinates` calculates the number of grid
+points it then multiplies the number of grid points by the spacing and adds the
+minimum values so that the points are spatially located.
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import verde as vd
+from matplotlib.patches import Rectangle
+
+# let's create some bounds for the region and set the spacing for the grid points
+spacing = 500
+west = 0
+east = 1555
+south = 0
+north = 1250
+region = (west, east, south, north)
+
+########################################################################################
+# Create the grid with adjust region
+# ----------------------------------
+#
+# With a region and spacing defined, grid_coordinates can now create a regular
+# grid. First let's pass a set shape to :func:`verde.grid_coordinates` to
+# confirm that it is creating 4 west-east grid points and 3 south-north grid
+# points, and set the adjust parameter to `region`
+
+regioneast, regionnorth = vd.grid_coordinates(
+    region=region, spacing=spacing, adjust="region"
+)
+print(regioneast.shape, regionnorth.shape)
+
+########################################################################################
+# With the spacing set at 500 units and a 3 by 4 grid of regular dimensions,
+# :func:`verde.grid_coordinates` then calculates the spatial location of each
+# grid point and adjusts the region so that the maximum northing and maximum
+# easting values are divisible by the spacing. In this example, the easting has
+# 3 segments (4 nodes) that are each 500 units long, meaning the easting spans
+# from 0 to 1500. The northing has 2 segments (3 nodes) that are each 500 units
+# long, meaning the northing spans from 0 to 1000. Both dimensions are divisble
+# by 500.
+
+print(regioneast)
+print(regionnorth)
+
+######################################################################################
+# By default if adjust is not assigned to `region` or `spacing`,
+# :func:`verde.grid_coordinates` will adjust the spacing. With the adjust
+# parameter set to `spacing` :func:`verde.grid_coordinates` creates grid nodes
+# in a similar manner as when it adjusts the region. However, it doesn't readjust
+# the region so that it is divisble by the spacing before creating the grid.
+# This means the grid will have the same number of grid points no matter if
+# `region` or `spacing` are set to the adjust parameter.
+
+########################################################################################
+# Create the grid with adjust spacing
+# -----------------------------------
+#
+# Now let's adjust the spacing of the grid points. Note that the number of grid
+# points is still the same as above.
+
+spacingeast, spacingnorth = vd.grid_coordinates(
+    region=region, spacing=500, adjust="spacing"
+)
+print(spacingeast.shape, spacingnorth.shape)
+
+######################################################################################
+# However the regular spacing between the grid points is no longer 500 units.
+
+print(spacingeast)
+print(spacingnorth)
+
+######################################################################################
+# Visualize the different adjustments
+# -----------------------------------
+# Let's visualize the difference between the original region bounds, the
+# adjusted region grid nodes, and the adjusted spacing grid nodes. Note the
+# adjusted spacing grid nodes keep the original region, while the adjusted
+# region grid nodes on the north and east side of the region have moved.
+
+plt.Rectangle((west, south), east, north)
+plt.figure(figsize=(8, 8))
+currentAxis = plt.gca()
+currentAxis.add_patch(
+    plt.Rectangle((west, south), east, north, fill=None, label="Region Bounds")
+)
+plt.scatter(regioneast, regionnorth, label="Adjusted Region Grid Nodes")
+plt.scatter(spacingeast, spacingnorth, label="Adjusted Spacing Grid Nodes")
+plt.Rectangle((west, south), east, north)
+plt.ylim(-50, 1600)
+plt.xlim(-50, 1600)
+plt.xlabel("Easting")
+plt.ylabel("Northing")
+plt.legend()
+plt.legend(bbox_to_anchor=(1.45, 1.0), loc=1, borderaxespad=0.0)
+plt.show()
