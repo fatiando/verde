8d243a36371224f5138a19fc7b9f1ad36dbd0b51
thedude <jessepisel@gmail.com>
2019-07-08 15:15:07 -0600

    updated tutorial

--- tutorials/grid_coordinates.py
+++ tutorials/grid_coordinates.py
@@ -11,13 +11,14 @@ dimension (the *shape*) or by the grid node spacing.
 
 Creating regular grids in Verde is done using the :func:`verde.grid_coordinates`
 function. The function uses :func:`numpy.linspace` and
-:func:`numpy.meshgrid` to interpolate between points in both the west-east and
-south-north directions to create a two-dimensional spatial grid. To this
-end, the function will adjust either the region bounds or the spacing between
-grid nodes.
+:func:`numpy.meshgrid` to create a set of regulary spaced points in both the
+west-east and south-north directions to create a two-dimensional spatial grid.
+To this end, the function will adjust either the region bounds or the spacing
+between grid nodes.
 
-First let's create a region that is 1555 units west-east and 1250 units south-
-north, and we will set an initial spacing to 500 units. 
+First let's create a region that is 1000 units west-east and 1000 units south-
+north, and we will set an initial spacing to 100 units. This should create a
+square grid with no adjustment of the region, or the spacing.
 """
 
 import numpy as np
@@ -25,45 +26,86 @@ import matplotlib.pyplot as plt
 import verde as vd
 from matplotlib.patches import Rectangle
 
-# create some bounds for the region and set the spacing for the grid points
-spacing = 500
-west = 0
-east = 1555
-south = 0
-north = 1250
+# create the bounds for the region and set the spacing for the grid points
+spacing = 100
+west, east, south, north = 0, 1000, 0, 1000
 region = (west, east, south, north)
+
+# create the grid
+square_region_east, square_region_north = vd.grid_coordinates(
+    region=region, spacing=spacing
+)
+
+########################################################################################
+# We can check the dimensions of the grid to confirm that it is 11x11
+
+print(square_region_east.shape, square_region_north.shape)
+
+########################################################################################
+# Now let's plot the grid to visualize the location of the grid points
+
+plt.figure(figsize=(6, 6))
+currentAxis = plt.gca()
+currentAxis.add_patch(
+    plt.Rectangle((west, south), east, north, fill=None, label="Region Bounds")
+)
+
+plt.vlines(
+    square_region_east[0],
+    ymin=min(square_region_north[0]),
+    ymax=max(square_region_north[-1]),
+    linestyles="dotted",
+    zorder=0,
+)
+plt.hlines(
+    square_region_north[:, 1],
+    xmin=min(square_region_east[0]),
+    xmax=max(square_region_east[-1]),
+    linestyles="dotted",
+    zorder=0,
+)
+
+plt.scatter(
+    square_region_east,
+    square_region_north,
+    label="Square Region Grid Nodes",
+    marker=".",
+    color="black",
+    s=100,
+)
+
+plt.ylim(-50, 1050)
+plt.xlim(-50, 1050)
+plt.xlabel("Easting")
+plt.ylabel("Northing")
+plt.legend(loc="upper center", bbox_to_anchor=(0.5, 1.15))
+plt.show()
 
 ########################################################################################
 # How Grid Coordinates works
 # --------------------------
-# When we choose to adjust the region, the function takes the original region and
-# divides the south-north and west-east extents of the region by the spacing to
-# get the number of grid points in both directions. This value is then
-# rounded and converted to an integer. The function then adds one to the integer
-# to get the number of nodes rather than segments.The function then multiplies
-# the number of grid points by the chosen spacing to create the new region.
-#
-# For the dimensions and spacing we defined above we
-# expect the west-east dimension to contain 4 grid points (1555/500 is 3.11 which
-# is rounded to 3 segments and 4 nodes), and the south-north dimensions to
-# contain 3 grid points (1250/500 is 2.5 which is rounded to 2 segments and 3
-# nodes. After :func:`verde.grid_coordinates` calculates the number of grid
-# points it then multiplies the number of grid points by the spacing and adds the
-# minimum values so that the points are spatially located.
+# Now let's create a region that is 1555 units west-east, and 1250 units south-north
+# with a spacing of 500 units. Because the range of the west-east and south-north
+# boundaries are not multiples of 500, we must choose to change either:
+#   - the boundaries of the region in order to fit the spacing, or
+#   - the spacing in order to fit the region boundaries.
+
+spacing = 500
+west, east, south, north = 0, 1555, 0, 1250
+region = (west, east, south, north)
 
 ########################################################################################
 # Create the grid with adjust region
 # ----------------------------------
-#
 # With a region and spacing defined, :func:`verde.grid_coordinates` can now create a regular
 # grid. First let's pass our data to :func:`verde.grid_coordinates` to
 # confirm that it is creating 4 west-east grid points and 3 south-north grid
 # points, and set the adjust parameter to ``region``
 
-regioneast, regionnorth = vd.grid_coordinates(
+region_east, region_north = vd.grid_coordinates(
     region=region, spacing=spacing, adjust="region"
 )
-print(regioneast.shape, regionnorth.shape)
+print(region_east.shape, region_north.shape)
 
 ########################################################################################
 # With the spacing set at 500 units and a 3 by 4 grid of regular dimensions,
@@ -75,8 +117,8 @@ print(regioneast.shape, regionnorth.shape)
 # long, meaning the northing spans from 0 to 1000. Both dimensions are divisible
 # by 500.
 
-print(regioneast)
-print(regionnorth)
+print(region_east)
+print(region_north)
 
 ######################################################################################
 # By default if adjust is not assigned to ``region`` or ``spacing``,
@@ -94,16 +136,16 @@ print(regionnorth)
 # Now let's adjust the spacing of the grid points. Note that the number of grid
 # points is still the same as above.
 
-spacingeast, spacingnorth = vd.grid_coordinates(
+spacing_east, spacing_north = vd.grid_coordinates(
     region=region, spacing=500, adjust="spacing"
 )
-print(spacingeast.shape, spacingnorth.shape)
+print(spacing_east.shape, spacing_north.shape)
 
 ######################################################################################
 # However the regular spacing between the grid points is no longer 500 units.
 
-print(spacingeast)
-print(spacingnorth)
+print(spacing_east)
+print(spacing_north)
 
 ######################################################################################
 # Visualize the different adjustments
@@ -116,27 +158,63 @@ print(spacingnorth)
 plt.figure(figsize=(6, 6))
 currentAxis = plt.gca()
 currentAxis.add_patch(
-    plt.Rectangle((west, south), east, north, fill=None, label="Region Bounds")
+    plt.Rectangle((west, south), east, north, fill=None, lw=2.0, label="Region Bounds")
 )
+
+plt.vlines(
+    region_east[0],
+    ymin=min(region_north[0]),
+    ymax=max(region_north[-1]),
+    linestyles="dotted",
+    zorder=0,
+)
+plt.hlines(
+    region_north[:, 1],
+    xmin=min(region_east[0]),
+    xmax=max(region_east[-1]),
+    linestyles="dotted",
+    zorder=0,
+)
+
 plt.scatter(
-    regioneast,
-    regionnorth,
+    region_east,
+    region_north,
     label="Adjusted Region Grid Nodes",
     marker=">",
     color="blue",
+    alpha=0.75,
+    s=100,
+)
+
+plt.vlines(
+    spacing_east[0],
+    ymin=min(spacing_north[0]),
+    ymax=max(spacing_north[-1]),
+    linestyles="dashed",
+    zorder=0,
+)
+plt.hlines(
+    spacing_north[:, 1],
+    xmin=min(spacing_east[0]),
+    xmax=max(spacing_east[-1]),
+    linestyles="dashed",
+    zorder=0,
 )
 plt.scatter(
-    spacingeast,
-    spacingnorth,
+    spacing_east,
+    spacing_north,
     label="Adjusted Spacing Grid Nodes",
     marker=">",
     color="orange",
+    alpha=0.75,
+    s=100,
 )
-plt.ylim(-50, 1600)
+
+plt.ylim(-50, 1300)
 plt.xlim(-50, 1600)
 plt.xlabel("Easting")
 plt.ylabel("Northing")
-plt.legend(loc="upper center")
+plt.legend(loc="upper center", bbox_to_anchor=(0.5, 1.18))
 plt.show()
 
 ######################################################################################
@@ -144,38 +222,41 @@ plt.show()
 # ------------------
 # Pixel registration locates the grid points in the middle of the grid segments
 # rather than in the corner of each grid.
-# First, let's take the same region and grid, and set the `adjust` parameter to
-# ``region`` so that the function will adjust the region, and set
-# ``pixel_register`` parameter to `True`. Without pixel registration our grid
-# had dimensions of 3x4, with pixel registration we expect the dimensions of
-# the grid to be the dimensions of the non-registered grid minus one or equal to
-# the number of segments between the grid points in the non-registered grid.
+# First, let's take the 1000x1000 region with 100 unit spacing from the first example
+# and set the ``pixel_register`` parameter to `True`. Without pixel registration our
+# grid should have dimensions of 11x11. With pixel registration we expect the dimensions
+# of the grid to be the dimensions of the non-registered grid minus one, or equal to
+# the number of segments between the grid points in the non-registered grid (10x10).
 
-regioneastpixel, regionnorthpixel = vd.grid_coordinates(
-    region=region, spacing=spacing, adjust="region", pixel_register=True
+spacing = 100
+west, east, south, north = 0, 1000, 0, 1000
+region = (west, east, south, north)
+
+pixel_east, pixel_north = vd.grid_coordinates(
+    region=region, spacing=spacing, pixel_register=True
 )
-print(regioneastpixel.shape, regionnorthpixel.shape)
+print(pixel_east.shape, pixel_north.shape)
 
 ######################################################################################
-# And we can check the coordinates for the grid points with region adjustment.
+# And we can check the coordinates for the grid points with pixel registration.
 
-print(regioneastpixel)
-print(regionnorthpixel)
+print(pixel_east)
+print(pixel_north)
 
 ######################################################################################
-# If we set the ``adjust`` parameter to ``spacing`` the function will return
-# the same dimensions as when ``adjust`` is set to ``region``.
+# If we set ``pixel_register`` to ``False`` the function will return the grid
+# with one more grid node in both west-east and south-north directions.
 
-spacingeastpixel, spacingnorthpixel = vd.grid_coordinates(
-    region=region, spacing=500, adjust="spacing", pixel_register=True
+no_pixel_east, no_pixel_north = vd.grid_coordinates(
+    region=region, spacing=spacing, pixel_register=False
 )
-print(spacingeastpixel.shape, spacingnorthpixel.shape)
+print(no_pixel_east.shape, no_pixel_north.shape)
 
 ######################################################################################
 # Again we can check the coordinates for grid points with spacing adjustment.
 
-print(spacingeastpixel)
-print(spacingnorthpixel)
+print(no_pixel_east)
+print(no_pixel_north)
 
 ######################################################################################
 # Lastly, we can plot up the pixel-registered grid points to see where they fall
@@ -184,25 +265,60 @@ print(spacingnorthpixel)
 plt.figure(figsize=(6, 6))
 currentAxis = plt.gca()
 currentAxis.add_patch(
-    plt.Rectangle((west, south), east, north, fill=None, label="Region Bounds")
+    plt.Rectangle((west, south), east, north, fill=None, lw=2.0, label="Region Bounds")
 )
+plt.vlines(
+    pixel_east[0],
+    ymin=min(pixel_north[0]),
+    ymax=max(pixel_north[-1]),
+    linestyles="dotted",
+    zorder=0,
+)
+plt.hlines(
+    pixel_north[:, 1],
+    xmin=min(pixel_east[0]),
+    xmax=max(pixel_east[-1]),
+    linestyles="dotted",
+    zorder=0,
+)
+
 plt.scatter(
-    regioneastpixel,
-    regionnorthpixel,
-    label="Adjust Region Grid Nodes",
+    pixel_east,
+    pixel_north,
+    label="Pixel Registered Grid Nodes",
     marker="o",
     color="blue",
+    alpha=0.75,
+    s=100,
+)
+
+plt.vlines(
+    no_pixel_east[0],
+    ymin=min(no_pixel_north[0]),
+    ymax=max(no_pixel_north[-1]),
+    linestyles="dashed",
+    zorder=0,
+)
+plt.hlines(
+    no_pixel_north[:, 1],
+    xmin=min(no_pixel_east[0]),
+    xmax=max(no_pixel_east[-1]),
+    linestyles="dashed",
+    zorder=0,
 )
 plt.scatter(
-    spacingeastpixel,
-    spacingnorthpixel,
-    label="Adjust Spacing Grid Nodes",
+    no_pixel_east,
+    no_pixel_north,
+    label="Regular Registered Grid Nodes",
     marker="o",
     color="orange",
+    alpha=0.75,
+    s=100,
 )
-plt.ylim(-50, 1600)
-plt.xlim(-50, 1600)
+
+plt.ylim(-50, 1050)
+plt.xlim(-50, 1050)
 plt.xlabel("Easting")
 plt.ylabel("Northing")
-plt.legend(loc="upper center")
+plt.legend(loc="upper center", bbox_to_anchor=(0.5, 1.18))
 plt.show()
