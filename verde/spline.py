f91059c8887dd99c278a9b111ec64efdb2c57739
Leonardo Uieda <leouieda@gmail.com>
2020-01-21 20:49:38 +0000

    Explicitly set numba config for each jit function (#221)
    Using `parallel=True` on the Green's function was raising a warning
    because there is nothing to parallelize. Set arguments individually to
    avoid this. Add missing `numba.prange` to Spline Jacobian calculation to
    actually run it in parallel. A combination of Dask + numba makes some 
    tests hang on CI randomly even if passing locally.Run the tests that use 
    dask with the numpy engine to avoid this.

--- verde/spline.py
+++ verde/spline.py
@@ -23,9 +23,6 @@ except ImportError:
 # Otherwise, DeprecationWarning won't be shown, kind of defeating the purpose.
 warnings.simplefilter("default")
 
-# Default arguments for numba.jit
-JIT_ARGS = dict(nopython=True, target="cpu", fastmath=True, parallel=True)
-
 
 class SplineCV(BaseGridder):
     r"""
@@ -538,7 +535,7 @@ def jacobian_numpy(east, north, force_east, force_north, mindist, jac):
     return jac
 
 
-@jit(**JIT_ARGS)
+@jit(nopython=True, fastmath=True, parallel=True)
 def predict_numba(east, north, force_east, force_north, mindist, forces, result):
     "Calculate the predicted data using numba to speed things up."
     for i in numba.prange(east.size):  # pylint: disable=not-an-iterable
@@ -551,10 +548,10 @@ def predict_numba(east, north, force_east, force_north, mindist, forces, result)
     return result
 
 
-@jit(**JIT_ARGS)
+@jit(nopython=True, fastmath=True, parallel=True)
 def jacobian_numba(east, north, force_east, force_north, mindist, jac):
     "Calculate the Jacobian matrix using numba to speed things up."
-    for i in range(east.size):
+    for i in numba.prange(east.size):  # pylint: disable=not-an-iterable
         for j in range(force_east.size):
             jac[i, j] = GREENS_FUNC_JIT(
                 east[i] - force_east[j], north[i] - force_north[j], mindist
@@ -563,4 +560,4 @@ def jacobian_numba(east, north, force_east, force_north, mindist, jac):
 
 
 # Jit compile the Green's functions for use in the numba functions
-GREENS_FUNC_JIT = jit(**JIT_ARGS)(greens_func)
+GREENS_FUNC_JIT = jit(nopython=True, fastmath=True)(greens_func)
