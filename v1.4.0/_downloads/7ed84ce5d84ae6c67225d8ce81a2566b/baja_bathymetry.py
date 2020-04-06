b020b5e2fafd1beddb3f4a70d162f42a5cc65454
Leonardo Uieda <leouieda@gmail.com>
2020-04-06 17:59:42 +0100

    Deploy v1.4.0 manually since TravisCI failed
    It was supposed to support a new parameter "deploy.cleanup: false"
    instead of "deploy.skip_cleanup: true" but it didn't. So here we are.

---
+++ v1.4.0/_downloads/7ed84ce5d84ae6c67225d8ce81a2566b/baja_bathymetry.py
@@ -0,0 +1,36 @@
+"""
+Bathymetry data from Baja California
+====================================
+
+We provide sample bathymetry data from Baja California to test the gridding
+methods. This is the ``@tut_ship.xyz`` sample data from the `GMT
+<http://gmt.soest.hawaii.edu/>`__ tutorial. The data is downloaded to a local
+directory if it's not there already.
+"""
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
+import verde as vd
+
+
+# The data are in a pandas.DataFrame
+data = vd.datasets.fetch_baja_bathymetry()
+print(data.head())
+
+# Make a Mercator map of the data using Cartopy
+plt.figure(figsize=(7, 6))
+ax = plt.axes(projection=ccrs.Mercator())
+ax.set_title("Bathymetry from Baja California")
+# Plot the bathymetry as colored circles. Cartopy requires setting the projection of the
+# original data through the transform argument. Use PlateCarree for geographic data.
+plt.scatter(
+    data.longitude,
+    data.latitude,
+    c=data.bathymetry_m,
+    s=0.1,
+    transform=ccrs.PlateCarree(),
+)
+plt.colorbar().set_label("meters")
+# Use an utility function to add tick labels and land and ocean features to the map.
+vd.datasets.setup_baja_bathymetry_map(ax)
+plt.tight_layout()
+plt.show()
---
+++ v1.4.0/_sources/api/generated/verde.train_test_split.rst.txt
@@ -0,0 +1,12 @@
+verde.train\_test\_split
+========================
+
+.. currentmodule:: verde
+
+.. autofunction:: train_test_split
+
+.. include:: backreferences/verde.train_test_split.examples
+
+.. raw:: html
+
+     <div style='clear:both'></div>
