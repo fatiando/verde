3fd63935c9e5f7e398ed3918ea238df124faa322
Santiago Soler <santiago.r.soler@gmail.com>
2021-04-08 10:08:02 -0300

    Extend support for Python 3.9

--- setup.py
+++ setup.py
@@ -35,6 +35,7 @@ CLASSIFIERS = [
     "Programming Language :: Python :: 3.6",
     "Programming Language :: Python :: 3.7",
     "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
     "Programming Language :: Python :: 3 :: Only",
     "License :: OSI Approved :: {}".format(LICENSE),
 ]
