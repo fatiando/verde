e971e2376d255263e0424256766ecd84a1cc0012
Leonardo Uieda <leouieda@gmail.com>
2021-12-10 15:23:59 +0000

    Update setuptools_scm configuration

---
+++ verde/_version.py
@@ -0,0 +1,12 @@
+# Copyright (c) 2017 The Verde Developers.
+# Distributed under the terms of the BSD 3-Clause License.
+# SPDX-License-Identifier: BSD-3-Clause
+#
+# This code is part of the Fatiando a Terra project (https://www.fatiando.org)
+
+# This file is generated automatically by setuptools_scm
+from . import _version_generated
+
+
+# Add a "v" to the version number made by setuptools_scm
+__version__ = f"v{_version_generated.version}"
