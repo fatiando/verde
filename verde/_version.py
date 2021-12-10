0b999a97603784dcbe37cd78340602c041e0fb97
Leonardo Uieda <leouieda@gmail.com>
2021-12-10 15:33:23 +0000

    Add docstring for the _version module

--- verde/_version.py
+++ verde/_version.py
@@ -4,11 +4,11 @@
 #
 # This code is part of the Fatiando a Terra project (https://www.fatiando.org)
 #
-# Copyright (c) 2017 The Verde Developers.
-# Distributed under the terms of the BSD 3-Clause License.
-# SPDX-License-Identifier: BSD-3-Clause
-#
-# This code is part of the Fatiando a Terra project (https://www.fatiando.org)
+# pylint: disable=no-name-in-module
+"""
+Get the automatically generated version information from setuptools_scm and
+format it nicely.
+"""
 
 # This file is generated automatically by setuptools_scm
 from . import _version_generated
