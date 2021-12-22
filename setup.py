51f8818ef70950fb1345597e4c28406085d4ddee
Leonardo Uieda <leouieda@gmail.com>
2021-12-22 06:02:01 +0000

    Move configuration from setup.py to setup.cfg (#348)
    Make the move away from setup.py following the recommendations from the
    Python packaging guides. Moves the requirement listing to setup.cfg as
    well and will use a script to extract this for conda installing on CI.

--- setup.py
+++ setup.py
@@ -5,78 +5,11 @@
 # This code is part of the Fatiando a Terra project (https://www.fatiando.org)
 #
 """
-Build and install the project.
-
+Setup script for the Python package.
+Metadata and build configuration are defined in setup.cfg
 Uses setuptools-scm to manage version numbers using git tags.
 """
-from setuptools import find_packages, setup
-
-NAME = "verde"
-FULLNAME = "Verde"
-AUTHOR = "The Verde Developers"
-AUTHOR_EMAIL = "leouieda@gmail.com"
-MAINTAINER = "Leonardo Uieda"
-MAINTAINER_EMAIL = AUTHOR_EMAIL
-LICENSE = "BSD License"
-URL = "https://github.com/fatiando/verde"
-DESCRIPTION = "Processing and gridding spatial data"
-KEYWORDS = ""
-with open("README.rst") as f:
-    LONG_DESCRIPTION = "".join(f.readlines())
-CLASSIFIERS = [
-    "Development Status :: 5 - Production/Stable",
-    "Intended Audience :: Science/Research",
-    "Intended Audience :: Developers",
-    "Intended Audience :: Education",
-    "Topic :: Scientific/Engineering",
-    "Topic :: Software Development :: Libraries",
-    "Programming Language :: Python :: 3",
-    "Programming Language :: Python :: 3.6",
-    "Programming Language :: Python :: 3.7",
-    "Programming Language :: Python :: 3.8",
-    "Programming Language :: Python :: 3.9",
-    "Programming Language :: Python :: 3 :: Only",
-    "License :: OSI Approved :: {}".format(LICENSE),
-]
-PLATFORMS = "Any"
-PACKAGES = find_packages(exclude=["doc"])
-SCRIPTS = []
-PACKAGE_DATA = {
-    "verde.datasets": ["registry.txt"],
-    "verde.tests": ["data/*", "baseline/*"],
-}
-with open("requirements.txt") as f:
-    INSTALL_REQUIRES = f.readlines()
-PYTHON_REQUIRES = ">=3.6"
-# Configuration for setuptools-scm
-SETUP_REQUIRES = ["setuptools_scm"]
-USE_SCM_VERSION = {
-    "relative_to": __file__,
-    "version_scheme": "post-release",
-    "local_scheme": "node-and-date",
-    "write_to": f"{NAME}/_version_generated.py",
-}
+from setuptools import setup
 
 if __name__ == "__main__":
-    setup(
-        name=NAME,
-        fullname=FULLNAME,
-        description=DESCRIPTION,
-        long_description=LONG_DESCRIPTION,
-        use_scm_version=USE_SCM_VERSION,
-        author=AUTHOR,
-        author_email=AUTHOR_EMAIL,
-        maintainer=MAINTAINER,
-        maintainer_email=MAINTAINER_EMAIL,
-        license=LICENSE,
-        url=URL,
-        platforms=PLATFORMS,
-        scripts=SCRIPTS,
-        packages=PACKAGES,
-        package_data=PACKAGE_DATA,
-        classifiers=CLASSIFIERS,
-        keywords=KEYWORDS,
-        install_requires=INSTALL_REQUIRES,
-        python_requires=PYTHON_REQUIRES,
-        setup_requires=SETUP_REQUIRES,
-    )
+    setup()
