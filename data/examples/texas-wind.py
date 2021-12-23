10eb69bb7f7144d43257a8fc2afd9833153129ec
Santiago Soler <santiago.r.soler@gmail.com>
2021-12-23 10:13:50 -0300

    Deprecate the sample datasets (#277)
    Sample data will be removed in Verde 2.0.0 in favour of using 
    the Ensaio library instead. Add a deprecation warning to all
    fetching and plotting functions and warnings to the documentation
    and docstrings.
    
    Co-authored-by: Leonardo Uieda <leouieda@gmail.com>

--- data/examples/texas-wind.py
+++ data/examples/texas-wind.py
@@ -5,8 +5,15 @@
 # This code is part of the Fatiando a Terra project (https://www.fatiando.org)
 #
 """
-Wind speed data from Texas
-==========================
+[DEPRECATED] Wind speed data from Texas
+=======================================
+
+.. warning::
+
+    All sample datasets in Verde are deprecated and will be
+    **removed in Verde v2.0.0**.
+    The tutorials/examples will transition to using
+    `Ensaio <https://www.fatiando.org/ensaio/>`__ instead.
 
 This is average wind speed and air temperature for data for the state of Texas,
 USA, on February 26 2018. The original data was downloaded from `Iowa State
