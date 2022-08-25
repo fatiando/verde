799190f8d7779492becf13db567fcb4f9549b762
Leonardo Uieda <leouieda@gmail.com>
2022-08-25 16:11:19 +0100

    Add a Linear gridder class based on SciPy
    This class replaces `ScipyGridder(method="linear")` to provide a nicer
    looking interface. It'll also allow us to deprecate `ScipyGridder`
    and add an `extrapolate` keyword argument in the future to replace the
    NaNs outside of the convex hull with nearest neighbor values.

--- verde/scipygridder.py
+++ verde/scipygridder.py
@@ -5,8 +5,9 @@
 # This code is part of the Fatiando a Terra project (https://www.fatiando.org)
 #
 """
-A gridder that uses scipy.interpolate as the backend.
+Gridders that use scipy.interpolate as the backend.
 """
+from abc import abstractmethod
 from warnings import warn
 
 import numpy as np
@@ -21,7 +22,135 @@ from .base import BaseGridder, check_fit_input
 from .coordinates import get_region
 
 
-class ScipyGridder(BaseGridder):
+class _BaseScipyGridder(BaseGridder):
+    """
+    A scipy.interpolate base gridder for scalar Cartesian data.
+
+    Used as a base class for each of the SciPy ND based interpolators.
+
+    Attributes
+    ----------
+    interpolator_ : scipy interpolator class
+        An instance of the corresponding scipy interpolator class.
+    region_ : tuple
+        The boundaries (``[W, E, S, N]``) of the data used to fit the
+        interpolator. Used as the default region for the
+        :meth:`~verde.ScipyGridder.grid` and
+        :meth:`~verde.ScipyGridder.scatter` methods.
+
+    """
+
+    @abstractmethod
+    def _get_interpolator(self):
+        """
+        Return the SciPy interpolator class and any extra keyword arguments as
+        a dictionary.
+        """
+
+    def fit(self, coordinates, data, weights=None):
+        """
+        Fit the interpolator to the given data.
+
+        The data region is captured and used as default for the
+        :meth:`~verde._BaseScipyGridder.grid` method.
+
+        Parameters
+        ----------
+        coordinates : tuple of arrays
+            Arrays with the coordinates of each data point. Should be in the
+            following order: (easting, northing, vertical, ...). Only easting
+            and northing will be used, all subsequent coordinates will be
+            ignored.
+        data : array
+            The data values that will be interpolated.
+        weights : None or array
+            Data weights are **not supported** by this interpolator and will be
+            ignored. Only present for compatibility with other gridder.
+
+        Returns
+        -------
+        self
+            Returns this gridder instance for chaining operations.
+
+        """
+        if weights is not None:
+            warn(
+                "{} does not support weights and they will be ignored.".format(
+                    self.__class__.__name__
+                )
+            )
+        coordinates, data, weights = check_fit_input(coordinates, data, weights)
+        easting, northing = coordinates[:2]
+        self.region_ = get_region((easting, northing))
+        points = np.column_stack((np.ravel(easting), np.ravel(northing)))
+        interpolator_class, kwargs = self._get_interpolator()
+        self.interpolator_ = interpolator_class(points, np.ravel(data), **kwargs)
+        return self
+
+    def predict(self, coordinates):
+        """
+        Interpolate data on the given set of points.
+
+        Requires a fitted gridder (see :meth:`~verde._BaseScipyGridder.fit`).
+
+        Parameters
+        ----------
+        coordinates : tuple of arrays
+            Arrays with the coordinates of each data point. Should be in the
+            following order: (easting, northing, vertical, ...). Only easting
+            and northing will be used, all subsequent coordinates will be
+            ignored.
+
+        Returns
+        -------
+        data : array
+            The data values interpolated on the given points.
+
+        """
+        check_is_fitted(self, ["interpolator_"])
+        easting, northing = coordinates[:2]
+        return self.interpolator_((easting, northing))
+
+
+class Linear(_BaseScipyGridder):
+    """
+    A piecewise linear interpolation.
+
+    Provides a Verde interface to
+    :class:`scipy.interpolate.LinearNDInterpolator`.
+
+    Parameters
+    ----------
+    rescale : bool
+        If ``True``, rescale the data coordinates to [0, 1] range before
+        interpolation. Useful when coordinates vary greatly in scale. Default
+        is ``True``.
+
+    Attributes
+    ----------
+    interpolator_ : scipy interpolator class
+        An instance of the corresponding scipy interpolator class.
+    region_ : tuple
+        The boundaries (``[W, E, S, N]``) of the data used to fit the
+        interpolator. Used as the default region for the
+        :meth:`~verde.Linear.grid` and
+        :meth:`~verde.Linear.scatter` methods.
+
+    """
+
+    def __init__(self, rescale=True):
+        super().__init__()
+        self.rescale = rescale
+
+    def _get_interpolator(self):
+        """
+        Return the SciPy interpolator class and any extra keyword arguments as
+        a dictionary.
+        """
+        return LinearNDInterpolator, {"rescale": self.rescale}
+
+
+class ScipyGridder(_BaseScipyGridder):
     """
     A scipy.interpolate based gridder for scalar Cartesian data.
 
@@ -57,37 +186,10 @@ class ScipyGridder(BaseGridder):
         self.method = method
         self.extra_args = extra_args
 
-    def fit(self, coordinates, data, weights=None):
+    def _get_interpolator(self):
         """
-        Fit the interpolator to the given data.
-
-        Any keyword arguments passed as the ``extra_args`` attribute will be
-        used when instantiating the scipy interpolator.
-
-        The data region is captured and used as default for the
-        :meth:`~verde.ScipyGridder.grid` and
-        :meth:`~verde.ScipyGridder.scatter` methods.
-
-        Parameters
-        ----------
-        coordinates : tuple of arrays
-            Arrays with the coordinates of each data point. Should be in the
-            following order: (easting, northing, vertical, ...). Only easting
-            and northing will be used, all subsequent coordinates will be
-            ignored.
-        data : array
-            The data values that will be interpolated.
-        weights : None or array
-            If not None, then the weights assigned to each data point.
-            Typically, this should be 1 over the data uncertainty squared.
-            Ignored for this interpolator. Only present for compatibility with
-            other gridder.
-
-        Returns
-        -------
-        self : verde.ScipyGridder
-            Returns this gridder instance for chaining operations.
-
+        Return the SciPy interpolator class and any extra keyword arguments as
+        a dictionary.
         """
         classes = dict(
             linear=LinearNDInterpolator,
@@ -104,39 +206,4 @@ class ScipyGridder(BaseGridder):
             kwargs = {}
         else:
             kwargs = self.extra_args
-        if weights is not None:
-            warn(
-                "{} does not support weights and they will be ignored.".format(
-                    self.__class__.__name__
-                )
-            )
-        coordinates, data, weights = check_fit_input(coordinates, data, weights)
-        easting, northing = coordinates[:2]
-        self.region_ = get_region((easting, northing))
-        points = np.column_stack((np.ravel(easting), np.ravel(northing)))
-        self.interpolator_ = classes[self.method](points, np.ravel(data), **kwargs)
-        return self
-
-    def predict(self, coordinates):
-        """
-        Interpolate data on the given set of points.
-
-        Requires a fitted gridder (see :meth:`~verde.ScipyGridder.fit`).
-
-        Parameters
-        ----------
-        coordinates : tuple of arrays
-            Arrays with the coordinates of each data point. Should be in the
-            following order: (easting, northing, vertical, ...). Only easting
-            and northing will be used, all subsequent coordinates will be
-            ignored.
-
-        Returns
-        -------
-        data : array
-            The data values interpolated on the given points.
-
-        """
-        check_is_fitted(self, ["interpolator_"])
-        easting, northing = coordinates[:2]
-        return self.interpolator_((easting, northing))
+        return classes[self.method], kwargs
