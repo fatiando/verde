2f5387741a4da473b90e55c119792b0d1c7d4c27
Santiago Soler <santisoler@fastmail.com>
2024-07-23 15:33:56 -0300

    Use pytest parametrize to run tests over regions

--- verde/tests/test_coordinates.py
+++ verde/tests/test_coordinates.py
@@ -75,21 +75,23 @@ def test_rolling_window_no_shape_or_spacing():
         rolling_window(coords, size=2)
 
 
-def test_rolling_window_oversized_window():
+@pytest.mark.parametrize(
+    "region",
+    [
+        (-5, -1, 6, 20),  # window larger than west-east
+        (-20, -1, 6, 10),  # window larger than south-north
+        (-5, -1, 6, 10),  # window larger than both dims
+    ],
+)
+def test_rolling_window_oversized_window(region):
     """
     Check if error is raised if size larger than region is passed
     """
     oversize = 5
-    regions = [
-        (-5, -1, 6, 20),  # window larger than west-east
-        (-20, -1, 6, 10),  # window larger than south-north
-        (-5, -1, 6, 10),  # window larger than both dims
-    ]
-    for region in regions:
-        coords = grid_coordinates(region, spacing=1)
-        err_msg = f"Window size '{oversize}' is larger than dimensions of the region "
-        with pytest.raises(ValueError, match=err_msg):
-            rolling_window(coords, size=oversize, spacing=2)
+    coords = grid_coordinates(region, spacing=1)
+    err_msg = f"Window size '{oversize}' is larger than dimensions of the region "
+    with pytest.raises(ValueError, match=err_msg):
+        rolling_window(coords, size=oversize, spacing=2)
 
 
 def test_spacing_to_size():
