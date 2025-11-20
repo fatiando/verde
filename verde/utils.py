44a27f7f82fbb7bd18eea6af5744ceb4d6f7afb9
mdtanker <matt.d.tankersley@gmail.com>
2025-11-20 09:17:08 +0100

    formatting

--- verde/utils.py
+++ verde/utils.py
@@ -247,9 +247,13 @@ def maxabs(*args, nan=True, percentile=100):
         return npmax(absolute)
     else:
         if not isinstance(percentile, (int, float)):
-            raise TypeError(f"Invalid 'percentile' of type '{type(percentile).__name__}'. Percentile must be a float or an integer.")
+            raise TypeError(
+                f"Invalid 'percentile' of type '{type(percentile).__name__}'. Percentile must be a float or an integer."
+            )
         if percentile < 0 or percentile > 100:
-            raise ValueError(f"Invalid 'percentile' value of '{percentile}'. It must be between 0 and 100.")
+            raise ValueError(
+                f"Invalid 'percentile' value of '{percentile}'. It must be between 0 and 100."
+            )
         if nan:
             nppercentile = np.nanpercentile
         else:
